356fdd1ef3c20bc511fbac7b4449d7453ad2d48d
Anand A <aanand_ub@yahoo.com>
2023-10-07 17:58:18 -0400

    20231007-175818

--- init.lua
+++ init.lua
@@ -9,19 +9,24 @@ vim.opt.rtp:prepend(lazypath)
 -- :Inspect
 -- :CocInstall coc-clangd coc-css coc-html coc-json coc-lua coc-pyright coc-rust-analyzer coc-tsserver
 -- Coc key bindings C-n C-p C-y C-e C-j C-k
--- :CocConfig json settings diagnostic.enable, clangd.enabled, pyright.inlayHints.enable
 
 require('lazy').setup({
-  {'catppuccin/nvim', priority = 1000, config = function() vim.cmd.colorscheme 'catppuccin' end},
+  {'navarasu/onedark.nvim'},
   {'rmehri01/onenord.nvim'},
   {'neoclide/coc.nvim', branch = 'master', build = 'npm ci'},
   {'numToStr/Comment.nvim', opts = {}},
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  },
   {'nvim-telescope/telescope.nvim', dependencies = {'nvim-lua/plenary.nvim'}},
   {'nvim-treesitter/nvim-treesitter', dependencies = {'nvim-treesitter/nvim-treesitter-textobjects',}, build = ':TSUpdate',},
   {'phaazon/hop.nvim'},
+  {'stevearc/oil.nvim', opts = {}, dependencies = { "nvim-tree/nvim-web-devicons" },},
 })
-require('telescope').setup {pickers = {find_files = {mappings={i={['<CR>']=require('telescope.actions').file_tab}}}}}
 require'hop'.setup {keys = 'etovxqpdygfblzhckisuran', term_seq_bias = 0.5}
+require('oil').setup()
+require('telescope').setup {pickers = {find_files = {mappings={i={['<CR>']=require('telescope.actions').file_tab}}}}}
 require('nvim-treesitter.configs').setup {
   ensure_installed = {'c', 'cpp', 'javascript', 'latex', 'lua', 'python', 'rust', 'vimdoc'},
   sync_install = false,
@@ -37,29 +42,13 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
-Coc = 1
-Colorschemes = vim.fn.getcompletion('', 'color')
-math.randomseed(os.time())
-Csn = math.random(1000000)
-
-function CocToggle()
-  Coc = 1 - Coc
-  if Coc==1 then vim.cmd([[CocEnable]]) else vim.cmd([[CocDisable]]) end
-end
-
-function ToggleColorScheme()
-  if vim.o.background == 'dark' then
-    vim.o.background = 'light'
-  else
-    vim.o.background = 'dark'
-  end
-end
-
+-- automatically remove extra spaces
 vim.api.nvim_create_autocmd({'BufWritePre'}, {
   pattern = {'*'},
   command = [[%s/\s\+$//e]],
 })
 
+-- disable folding
 vim.api.nvim_create_autocmd({'BufWritePost' , 'BufEnter'}, {
   pattern = {'*'},
   callback = function(_) -- arg is ev
@@ -72,17 +61,15 @@ vim.api.nvim_create_autocmd({'BufWritePost' , 'BufEnter'}, {
 
 vim.cmd([[ let g:python_recommended_style = 0 ]])
 vim.cmd([[au Filetype python setlocal ts=2 sts=0 sw=2]])
-vim.cmd([[cabbr h tab help]])
-vim.cmd([[cabbr q qa]])
-vim.cmd([[let g:coc_snippet_next = '<c-j>']])
-vim.cmd([[let g:coc_snippet_prev = '<c-k>']])
+vim.cmd([[cabbr h tab help]]) -- open help in new tab
+-- vim.cmd([[let g:coc_snippet_next = '<c-j>']])
+-- vim.cmd([[let g:coc_snippet_prev = '<c-k>']])
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', {silent = true})
 vim.o.background='dark'
 vim.o.clipboard = 'unnamedplus'
 vim.o.expandtab = true
@@ -103,33 +90,38 @@ vim.opt.shell = '/usr/bin/fish'
 vim.wo.number = false
 vim.wo.relativenumber = false
 
-vim.keymap.set('n', '<C-j>', 'gj', {noremap = true})
-vim.keymap.set('n', '<C-k>', 'gk', {noremap = true})
-vim.keymap.set('n', '<C-w>c', '<cmd>bd!<CR>', {noremap = true})
+vim.keymap.set('n', '<leader>X', '<cmd>bd!<CR>', {noremap = true}) --force close window
+vim.keymap.set('n', '<leader>fb', ':tabnew<CR>:Oil /home/ashoka<CR>', {}) --file browser
+vim.keymap.set('n', '<leader>nw', '<Plug>(coc-diagnostic-next)', {noremap = true}) --next warning
+vim.keymap.set('n', '<leader>sf', function() require('telescope.builtin').find_files({cwd='/home/ashoka',}) end, {noremap = true}) --search file
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, {}) --search help
+vim.keymap.set('n', '<leader>x', '<cmd>bd<CR>', {noremap = true}) --close window
 vim.keymap.set('n', 'N', 'Nzz', {noremap = true})
 vim.keymap.set('n', 'n', 'nzz', {noremap = true})
-vim.keymap.set('n', '<leader>nw', '<Plug>(coc-diagnostic-next)', {noremap = true}) -- next color
-vim.keymap.set('n', '<leader>sf', function() require('telescope.builtin').find_files({cwd='/home/ashoka',}) end, {}) -- search file
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, {}) -- search help
-vim.keymap.set('n', '<leader>ta', CocToggle, {}) -- toggle autocomplete
-vim.keymap.set('n', '<leader>tc', '<cmd>lua ToggleColorScheme()<CR>', {noremap = true}) -- toggle color
-vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', {noremap = true}) -- toggle color
-vim.keymap.set('n', 'f', '<cmd>HopWord<CR>', {noremap = true})
-vim.keymap.set('v', 'f', '<cmd>HopWord<CR>', {noremap = true})
-vim.keymap.set({'n', 'v'}, ',', '<C-b>M', {noremap = true})
-vim.keymap.set({'n', 'v'}, 'gj', 'j', {noremap = true})
-vim.keymap.set({'n', 'v'}, 'gk', 'k', {noremap = true})
-vim.keymap.set({'n', 'v'}, 'j', 'gj', {noremap = true})
-vim.keymap.set({'n', 'v'}, 'k', 'gk', {noremap = true})
-vim.keymap.set({'n', 'v'}, 't', '<C-f>M', {noremap = true})
+vim.keymap.set({'n', 'v'}, '<C-j>', 'j', {noremap = true})
+vim.keymap.set({'n', 'v'}, '<C-k>', 'k', {noremap = true})
+vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', {silent = true})
+vim.keymap.set({'n', 'v'}, 'j', "v:count == 0 ? 'gj' : 'j'", { noremap=true, expr = true, silent = true })
+vim.keymap.set({'n', 'v'}, 'k', "v:count == 0 ? 'gk' : 'k'", { noremap=true, expr = true, silent = true })
+
+vim.keymap.set({'i','n'}, '<M-s>', '<Esc><cmd>w<CR>', {silent=true, noremap = true}) --save file
+vim.keymap.set({'n', 'v'}, ',', '<C-b>M', {noremap = true}) -- previous page
+vim.keymap.set({'n', 'v'}, 'f', '<cmd>HopWord<CR>', {noremap = true}) -- hop
+vim.keymap.set({'n', 'v'}, 't', '<C-f>M', {noremap = true}) -- next page
 
 -- not useful in general, not recommended
+
+Colorschemes = vim.fn.getcompletion('', 'color')
+Csn = 1
 
 function Start_up_func()
   print(Colorschemes[Csn], '--', Csn)
 end
 
 function ShowColorScheme()
+  if vim.v.count ~= 0 then
+    Csn = vim.v.count
+  end
   Csn = Csn% #Colorschemes + 1
   vim.cmd.colorscheme(Colorschemes[Csn])
   Csn = (Csn - 2 + #Colorschemes)% #Colorschemes + 1
@@ -138,27 +130,21 @@ function ShowColorScheme()
   timer:start(500, 0, vim.schedule_wrap(Start_up_func))
 end
 
-function SelectNextColorScheme()
-  if vim.v.count == 0 then
-    Csn = Csn%#Colorschemes + 1
+function ToggleColorScheme()
+  if vim.o.background == 'dark' then
+    vim.o.background = 'light'
+    Csn = 2
   else
-    Csn = vim.v.count
+    vim.o.background = 'dark'
+    Csn = 1
   end
   ShowColorScheme()
 end
 
-function SelectPrevColorScheme()
-  if vim.v.count == 0 then
-    Csn = (Csn - 2 + #Colorschemes)%#Colorschemes + 1
-  else
-    Csn = vim.v.count
-  end
-  ShowColorScheme()
-end
+ShowColorScheme()
 
-vim.keymap.set('n', '<leader>nc', '<cmd>lua SelectNextColorScheme()<CR>', {noremap = true}) -- next color
-vim.keymap.set('n', '<leader>pc', '<cmd>lua SelectPrevColorScheme()<CR>', {noremap = true}) -- previous color
-vim.keymap.set('n', '<leader>sc', '<cmd>lua ShowColorScheme()<CR>', {noremap = true}) -- show color
+vim.keymap.set('n', '<leader>sc', ShowColorScheme, {noremap = true}) -- show color
+vim.keymap.set('n', '<leader>tc', ToggleColorScheme, {noremap = true}) -- toggle color
 
 function AttachString()
   local start = vim.fn.search('---', 'bW') + 1
@@ -168,5 +154,6 @@ function AttachString()
   vim.cmd(start .. ',' .. end1 .. 's/$/' .. vim.fn.getreg('"') )
   vim.cmd('norm ' .. start .. 'Gd' .. end1 .. 'G')
 end
+
 vim.keymap.set('n', '<leader>as', AttachString, {noremap = true})
 
